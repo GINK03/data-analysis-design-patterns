6f2e6b1bb7bf01e045d79788d52613e9149b9ba3
gimpei <gim.kobayashi@gmail.com>
2017-08-27 21:12:26 +0900

    update

---
+++ fueleconomy.gov/pandas-python.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('./vehicles.csv')
+
+# make作成したメーカの数でソート
+df1 = df.filter(items=['make', 'year'])
+mgroup = df1.groupby(['make'])
+#['year'].apply(lambda x:list(x))
+df2 = pd.DataFrame( [ [m, len(group['year'].tolist())] for m,group in mgroup] )
+df2.columns = ['make', 'freq']
+print( df2.sort_values('freq', ascending=False).head() )
+
+# 車の平均の燃費（/年）
+df3 = df.filter(items=['make', 'fuelCost08'])
+mgroup = df3.groupby(['make'])
+df4 = pd.DataFrame( [ [m, group['fuelCost08'].mean() ] for m,group in mgroup] )
+df4.columns = ['make', 'fuelCost08']
+print( df4.sort_values('fuelCost08', ascending=True).head() )
+
+# 各メーカで燃費が良い車種トップ5
+df5 = df.filter(items=['make', 'model', 'fuelCost08'])
+mgroup = df5.groupby(['make'])
+
+for make, group in mgroup:
+  print(make)
+  arr = list(set( [(data[1], data[2]) for index, data in group.iterrows()] ))
+  arr = pd.DataFrame(arr)
+  arr.columns = ['model', 'fuelCost08']
+  print( arr.sort_values('fuelCost08').head() )
+
