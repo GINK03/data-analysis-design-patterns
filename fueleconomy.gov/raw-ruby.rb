a8cd25df6e83565a7888885a57998490acdaeae7
nardtree <gim.kobayashi@gmail.com>
2017-08-28 03:53:33 +0000

    add 💎

--- fueleconomy.gov/raw-ruby.rb
+++ fueleconomy.gov/raw-ruby.rb
@@ -1,14 +1,15 @@
 require 'csv'
 require 'ostruct'
+require 'set'
 
 keys, *scores = CSV.read('vehicles.csv')
-p keys
 
 df = scores.map { |x| 
   r = Hash[ *keys.zip(x).flatten ] 
   r
 }
 
+# make作成したメーカの数でソート
 df.map{ |x| 
   d = OpenStruct.new
   d.make = x['make'] 
@@ -28,4 +29,25 @@ df.map{ |x|
   print x.make, ' ', x.freq, "\n"
 }
 
-
+# 各メーカで燃費が良い車種トップ5 
+df.map{ |x| 
+  d = OpenStruct.new
+  d.make = x['make'] 
+  d.model = x['model'] 
+  d.cost = x['fuelCost08']
+  d
+}.group_by { |x| 
+  x.make
+}.map { |xs|
+  make, arr = xs
+  arr.map { |x| 
+    d = OpenStruct.new
+    d.model = x.model
+    d.cost = x.cost.to_i
+    d
+  }.to_set.to_a.sort_by { |x| 
+    x.cost * -1
+  }.slice(0..20).map { |x| 
+    print x.model, ' ', x.cost, "\n"
+  }
+}
